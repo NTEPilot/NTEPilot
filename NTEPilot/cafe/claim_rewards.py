123f7f19164f3915e95e74080c3e5e23961c2c11
longer-sausage <longer-sausage@outlook.com>
2026-06-16 18:40:59 +0800

    fix

--- NTEPilot/cafe/claim_rewards.py
+++ NTEPilot/cafe/claim_rewards.py
@@ -8,8 +8,14 @@ class ClaimRewards(UI):
     def run(self):
         logger.hr('CLAIM CAFE REWARDS', level=1)
         self.ui_goto(CAFE_PAGE)
-        self.device.click(CLAIM_CAFE)
-        self.wait_until_appear_then_click(CONFIRM_CLAIM)
+
+        while True:
+            self.device.screenshot()
+            if self.appear_then_click(CONFIRM_CLAIM):
+                break
+            if self.appear_then_click(CLAIM_CAFE):
+                self.device.sleep((1, 1.2))
+
         self.wait_until_appear(GET_ITEM)
         self.device.click((640, 500))
         self.ui_goto(MAIN_PAGE)
